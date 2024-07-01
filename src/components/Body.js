bc77923d1619ae403657dc72fd606c35e879e84c
arun <akrao03213@gmail.com>
2024-07-01 22:52:48 +0530

    use api instead of mockdata to fetch restro detail also created Shimmer

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import Restro_Card from "./Restro_Card";
-import Restro_list from "../utils/mockdata";
+import Shimmer from "./Shimmer";
+
 const Body= ()=>{
-  const [Restro_data, setRestro_data]=useState(Restro_list);
-  return (
+  const [Restro_data, setRestro_data]=useState([]);
+  useEffect(()=>{
+    fetchdata();
+  },[]);
+  const fetchdata= async ()=>{
+    const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    const json =await data.json();
+    setRestro_data(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+  }
+  return Restro_data.length ==0?<Shimmer/>: (
     <div className="Body px-48 bg-slate-400">
             <div className="Filter h-24 flex mx-4 items-center">
                 <button className="h-10 w-20 border-2 rounded-full items-center" onClick={()=>{
@@ -18,7 +27,7 @@ const Body= ()=>{
                 ))}
             </div>
             
-        </div>
+      </div>
     )
   }
 export default Body;
---
+++ src/components/Shimmer.js
@@ -0,0 +1,19 @@
+const Shimmer =()=>{
+    return (
+        <div className="Body px-48 h-screen p-20 bg-slate-400">
+            <div className="Restro_Card flex flex-wrap">
+                <div className="mx-5 mb-8 bg-slate-200 rounded-2xl h-72 w-[21rem]"></div>
+                <div className="mx-5 mb-8 bg-slate-200 rounded-2xl h-72 w-[21rem]"></div>
+                <div className="mx-5 mb-8 bg-slate-200 rounded-2xl h-72 w-[21rem]"></div>
+                <div className="mx-5 mb-8 bg-slate-200 rounded-2xl h-72 w-[21rem]"></div>
+                <div className="mx-5 mb-8 bg-slate-200 rounded-2xl h-72 w-[21rem]"></div>
+                <div className="mx-5 mb-8 bg-slate-200 rounded-2xl h-72 w-[21rem]"></div>
+                <div className="mx-5 mb-8 bg-slate-200 rounded-2xl h-72 w-[21rem]"></div>
+                <div className="mx-5 mb-8 bg-slate-200 rounded-2xl h-72 w-[21rem]"></div>
+            </div>
+            
+      </div>
+        
+    )
+}
+export default Shimmer;
