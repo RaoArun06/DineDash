2dae6551d05e24b01bd0a76942b7a481ac57ab90
arun <akrao03213@gmail.com>
2024-07-03 11:55:21 +0530

    create different routing path using react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,14 +3,35 @@ import ReactDOM from "react-dom";
 import Header from "./components/Header.js";
 import Footer from "./components/Footer.js";
 import Body from "./components/Body.js";
+import { createBrowserRouter, RouterProvider , Outlet } from "react-router-dom";
 const App = ()=>{
     return (
         <>
         <Header/>
-        <Body/>
+        <Outlet/>
         <Footer/>
         </>
     )
 }
+const rout = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+        children:[
+            {
+                path: "/",
+                element: <Body/>
+            }
+            ,{
+                path: "/about",
+                Component: () => <h1>About Us</h1>
+            }
+            ,{
+                path: "/contact",
+                Component: () => <h1>Contact Us</h1>
+            }
+        ]
+    }
+])
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<RouterProvider router={rout}/>);
