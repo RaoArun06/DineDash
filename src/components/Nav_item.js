f4018b11bfa13f1a8c55c25d258952fae43e0b9c
arun <akrao03213@gmail.com>
2024-07-04 20:55:05 +0530

    created useOnlineStatus custom hook to display online/offline status

--- src/components/Nav_item.js
+++ src/components/Nav_item.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 const Nav_item= ()=>{
+    const onlineStatus=useOnlineStatus();
     return(
         <div className="flex gap-9">
-            
+            {(onlineStatus===true)?<h1>Online 🟢</h1>:<h1>Offline 🔴</h1>}
             <Link to="/">Home</Link>
             <Link to="/contact">Contact Us</Link>
             <Link to="/about">About Us</Link>
